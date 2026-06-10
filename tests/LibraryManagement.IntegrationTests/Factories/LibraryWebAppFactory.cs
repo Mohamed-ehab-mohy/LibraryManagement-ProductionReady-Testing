ce6f8600bffe90179f6b47cc3203d636454e0b18
Mohamed Ehab Mohy <mohamedehab92005@gmail.com>
2026-06-11 02:41:56 +0300

    I-4 opt: raw SQL truncate instead of RemoveRange

--- tests/LibraryManagement.IntegrationTests/Factories/LibraryWebAppFactory.cs
+++ tests/LibraryManagement.IntegrationTests/Factories/LibraryWebAppFactory.cs
@@ -66,21 +66,9 @@ public class LibraryWebAppFactory : WebApplicationFactory<Program>, IAsyncLifeti
             .Options;
 
         using var db = new LibraryDbContext(options);
-        db.Loans.RemoveRange(db.Loans);
-        db.Books.RemoveRange(db.Books);
-        db.Members.RemoveRange(db.Members);
-        await db.SaveChangesAsync();
-
-        db.Books.AddRange(
-            new Book { Title = "Clean Code", Author = "Robert C. Martin", ISBN = "9780132350884", TotalCopies = 5, AvailableCopies = 5 },
-            new Book { Title = "Clean Architecture", Author = "Robert C. Martin", ISBN = "9780134494166", TotalCopies = 3, AvailableCopies = 3 },
-            new Book { Title = "The Pragmatic Programmer", Author = "David Thomas", ISBN = "9780135957059", TotalCopies = 4, AvailableCopies = 4 }
-        );
-        db.Members.AddRange(
-            new Member { FullName = "Ahmed Ali", Email = "ahmed@test.com", MembershipExpiryDate = DateTime.UtcNow.AddMonths(6), OutstandingFine = 0 },
-            new Member { FullName = "Sara Hassan", Email = "sara@test.com", MembershipExpiryDate = DateTime.UtcNow.AddMonths(3), OutstandingFine = 0 }
-        );
-        await db.SaveChangesAsync();
+        await db.Database.ExecuteSqlRawAsync("TRUNCATE TABLE Loans");
+        await db.Database.ExecuteSqlRawAsync("UPDATE Books SET AvailableCopies = TotalCopies");
+        await db.Database.ExecuteSqlRawAsync("UPDATE Members SET OutstandingFine = 0");
     }
 
     public new async Task DisposeAsync()
